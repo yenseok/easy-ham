3777d30561f372295e16731cd6ba0c28f8625ea7
Yen Seok <seokyeeun130@naver.com>
2025-11-13 10:54:37 +0900

    rm indexpost

--- backend/src/main/java/com/A105/prham/search/service/SearchService.java
+++ backend/src/main/java/com/A105/prham/search/service/SearchService.java
@@ -430,35 +430,6 @@ public class SearchService {
         }
     }
 
-    //단일 post 인덱싱
-    // public void indexPost(Post post) {
-    //     try {
-    //         String originalLink = null;
-    //
-    //         // 개별 채용 공고는 postId에 "_" 포함되게 만들었음
-    //         // mm 원문 링크는 생성 안함. 왜냐? 필요 없으니까.
-    //         if (!post.getPostId().contains("_")) {
-    //             try {
-    //                 //원본 post만 mattermost 링크 생성
-    //                 var postDto = postProcessorService.preprocess(post);
-    //                 originalLink = postDto.getOriginalLink();
-    //             } catch (Exception e) {
-    //                 log.warn("mm 링크 생성 실패: postId={}", post.getPostId());
-    //             }
-    //         } else {
-    //             log.debug("개별 채용 공곤은 mm 링크 없이 인덱싱: {}", post.getPostId());
-    //         }
-    //
-    //         PostIndexDocument document = PostIndexDocument.from(post, originalLink);
-    //
-    //         Index index = meilisearchClient.index("posts");
-    //         String json = objectMapper.writeValueAsString(List.of(document));
-    //
-    //         index.addDocuments(json);
-    //     } catch (Exception e) {
-    //         throw new RuntimeException("meiliserarch 인덱싱 실패", e);
-    //     }
-    // }
 
     /**
      * 여러 Post 일괄 인덱싱
@@ -481,31 +452,6 @@ public class SearchService {
         }
     }
 
-    // public void indexPosts(List<Post> posts) {
-    //     try {
-    //         Index index = meilisearchClient.index(INDEX_NAME);
-    //
-    //         List<PostIndexDocument> documents = posts.stream()
-    //             .map(post -> {
-    //                 String originalLink = null;
-    //                 if (!post.getPostId().contains("_")) {
-    //                     try {
-    //                         var postDto = postProcessorService.preprocess(post);
-    //                         originalLink = postDto.getOriginalLink();
-    //                     } catch (Exception e) {
-    //                         log.warn("mm링크 생성 실패: postId={}", post.getPostId());
-    //                     }
-    //                 }
-    //                 return PostIndexDocument.from(post, originalLink);
-    //             })
-    //             .collect(Collectors.toList());
-    //
-    //         String json = objectMapper.writeValueAsString(documents);
-    //         index.addDocuments(json);
-    //     } catch (Exception e) {
-    //         throw new RuntimeException("인덱싱 실패", e);
-    //     }
-    // }
 
     /**
      * Post 삭제
