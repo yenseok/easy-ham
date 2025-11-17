9ca47270861eeeef606ab8a26d63d60e124bf276
ppower-dev <smothchoco@gmail.com>
2025-11-17 16:09:36 +0900

    [S13P31A105-169] feat : 헤더 상단에 고정

--- frontend/src/components/layouts/Header/index.tsx
+++ frontend/src/components/layouts/Header/index.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
   ];
 
   return (
-    <header className="h-16 bg-white border-b border-gray-200 flex items-center justify-between px-4 md:px-8 relative">
+    <header className="sticky top-0 h-16 bg-white border-b border-gray-200 flex items-center justify-between px-4 md:px-8 relative z-50">
       {/* 좌측 로고 및 앱명 */}
       <div className="flex items-center gap-2">
         <button
