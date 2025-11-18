ebf68d9ecbeaecb3fb846687c8bd5d1c4c582cba
Yen Seok <seokyeeun130@naver.com>
2025-11-18 18:20:42 +0900

    feat:user repo

--- backend/src/main/java/com/A105/prham/user/repository/UserPositionRepository.java
+++ backend/src/main/java/com/A105/prham/user/repository/UserPositionRepository.java
@@ -5,6 +5,7 @@ import com.A105.prham.position.entity.Position;
 import com.A105.prham.user.entity.User;
 import com.A105.prham.user.entity.UserPosition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +16,13 @@ public interface UserPositionRepository extends JpaRepository<UserPosition, Long
     List<UserPosition> findByUser(User user);
 
     List<UserPosition> findByPosition(Position position);
+
+    // UserRepository.java
+
+    @Query("SELECT DISTINCT u FROM User u " +
+        "LEFT JOIN FETCH u.userPositions up " +
+        "LEFT JOIN FETCH up.position " +
+        "LEFT JOIN FETCH u.notificationSetting " +
+        "WHERE u.notificationSetting.jobAlertEnabled = true")
+    List<User> findUsersWithJobAlertEnabled();
 }
